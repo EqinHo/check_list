714cf68069b6b4b92dba1fed84821acb0524636f
Quyen Ho <quyen99@hotmail.com>
2024-10-07 11:38:48 +0200

    all good

---
+++ src/Checklist_API/Checklist_API/Data/migrations/20241007092956_first.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Checklist_API.data.migrations
+{
+    /// <inheritdoc />
+    public partial class first : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "JWTRole",
+                columns: table => new
+                {
+                    RoleName = table.Column<string>(type: "varchar(20)", maxLength: 20, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_JWTRole", x => x.RoleName);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                    FirstName = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    LastName = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    PhoneNumber = table.Column<string>(type: "varchar(8)", maxLength: 8, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Email = table.Column<string>(type: "varchar(30)", maxLength: 30, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    HashedPassword = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Salt = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    DateCreated = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    DateUpdated = table.Column<DateTime>(type: "datetime(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "CheckList",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                    UserId = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                    Title = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Description = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Status = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Priority = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    AssignedTo = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Comments = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    DueDate = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    DateCreated = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    DateUpdated = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    DateCompleted = table.Column<DateTime>(type: "datetime(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CheckList", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CheckList_User_UserId",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "JWTUserRole",
+                columns: table => new
+                {
+                    RoleName = table.Column<string>(type: "varchar(20)", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    UserId = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                    DateCreated = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    DateUpdated = table.Column<DateTime>(type: "datetime(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_JWTUserRole", x => new { x.RoleName, x.UserId });
+                    table.ForeignKey(
+                        name: "FK_JWTUserRole_JWTRole_RoleName",
+                        column: x => x.RoleName,
+                        principalTable: "JWTRole",
+                        principalColumn: "RoleName",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_JWTUserRole_User_UserId",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.InsertData(
+                table: "JWTRole",
+                column: "RoleName",
+                values: new object[]
+                {
+                    "Admin",
+                    "User"
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CheckList_UserId",
+                table: "CheckList",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_JWTUserRole_UserId",
+                table: "JWTUserRole",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CheckList");
+
+            migrationBuilder.DropTable(
+                name: "JWTUserRole");
+
+            migrationBuilder.DropTable(
+                name: "JWTRole");
+
+            migrationBuilder.DropTable(
+                name: "User");
+        }
+    }
+}
--- src/Checklist_API/Checklist_API/Features/Login/Controller/LoginController.cs
+++ src/Checklist_API/Checklist_API/Features/Login/Controller/LoginController.cs
@@ -9,11 +9,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace Checklist_API.Features.Login.Controller;
 [Route("api/v1/login")]
 [ApiController]
-<<<<<<< HEAD
-public class LoginController(AuthenticationService authService, TokenGenerator tokenGenerator, ILogger<LoginController> logger) 
-=======
 public class LoginController(AuthenticationService authService, TokenGenerator tokenGenerator, ILogger<LoginController> logger)
->>>>>>> 7dc864fa4bae6c37c62f623b39c9d8b749d98245
                             : ControllerBase
 {
     private readonly AuthenticationService _authService = authService;
@@ -21,19 +17,11 @@ public class LoginController(AuthenticationService authService, TokenGenerator t
     private readonly ILogger<LoginController> _logger = logger;
 
     [AllowAnonymous]
-<<<<<<< HEAD
-    //  POST https://localhost:7070/api/v1/users/register
-    [HttpPost]
-    public async Task<IActionResult> Login([FromBody] LoginDTO loginDTO)
-    {
-        _logger.LogInformation("User Logging in: {username}", loginDTO.UserName);
-=======
     // POST https://localhost:7070/api/v1/login
     [HttpPost]
     public async Task<IActionResult> Login([FromBody] LoginDTO loginDTO) // spør gpt om ok Task her
     {
         _logger.LogInformation("User logging in: {username}", loginDTO.UserName);
->>>>>>> 7dc864fa4bae6c37c62f623b39c9d8b749d98245
 
         var user = await _authService.AuthenticateUserAsync(loginDTO);
 
@@ -42,11 +30,7 @@ public class LoginController(AuthenticationService authService, TokenGenerator t
             return Unauthorized("Not Authorized");
         }
 
-<<<<<<< HEAD
-        var tokenString = await _tokenGenerator.GenerateJSONWebTokenASync(user);
-=======
         var tokenString = await _tokenGenerator.GenerateJSONWebTokenAsync(user);
->>>>>>> 7dc864fa4bae6c37c62f623b39c9d8b749d98245
         return Ok(new { token = tokenString });
     }
 }
